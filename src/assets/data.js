32ba59cf4978897d0824d570dfff54a2d3c82569
Preetiranjan Sahoo <preetiranjansahoo388@gmail.com>
2024-10-25 20:08:02 +0530

    Quiz App

---
+++ src/assets/data.js
@@ -0,0 +1,43 @@
+export const data =[
+    {
+        question: "Which method is used to remove the last element from an array?",
+        option1: "shift()",
+        option2: "pop()",
+        option3: "push()",
+        option4: "splice()",
+        ans: 2,
+    },
+    {
+        question: "Which of the following will evaluate to false?",
+        option1: "[] == false",
+        option2: "0 == false",
+        option3: " == false",
+        option4: "All of the above",
+        ans: 3,
+    },
+    {
+        question: " How can you check if a variable x is an array?",
+        option1: "x instanceof Array",
+        option2: "Array.isArray(x)",
+        option3: "typeof x === array",
+        option4: " Both A and B",
+        ans: 4,
+    },
+    {
+        question: "Which keyword is used to handle exceptions in JavaScript?",
+        option1: "try",
+        option2: "catch",
+        option3: "finally",
+        option4: "All of the above",
+        ans: 4,
+    },
+    {
+        question: "What does map() do in JavaScript?",
+        option1: "Executes a function for each element in an array and returns a new array.",
+        option2: "Filters the array based on a condition.",
+        option3: "Modifies the original array.",
+        option4: "Checks if at least one element satisfies a condition.",
+        ans: 1,
+    },
+
+];
